318a3eb6f6533c2544f204b50758dc3f887890b5
dopamane <dwc1295@gmail.com>
2021-02-26 10:01:20 -0500

    drafting soc

--- src/Soc.hs
+++ src/Soc.hs
@@ -1,18 +1,23 @@
 module Soc where
 
 import Clash.Prelude
-import Core
+import Core (FromCore(FromCore), ToCore(ToCore), core)
 import Ice40.Clock
 import Ice40.Osc
 import Ice40.Rgb
+import Ice40.Led
 
 ---------
 -- RGB --
 ---------
 type Rgb = ("red" ::: Bit, "green" ::: Bit, "blue" ::: Bit)
 
+
+
 rgb :: Signal dom ToMem -> Signal dom Rgb
-rgb = mealy rgbM mkRgb
+rgb mem = rgbPrim "0b0" "0b111111" "0b111111" "0b111111" (pure 1) (pure 1) r g b
+  where
+    (r, g, b, _) = led cs wr addr en exe
 ----------
 -- BIOS --
 ----------
@@ -40,10 +45,10 @@ concat4 b3 b2 b1 b0 = b3 ++# b2 ++# b1 ++# b0
 -- Lion SOC --
 --------------
 lion :: HiddenClockResetEnable dom => Signal dom Rgb
-lion = fromCore^.toMem.to rgb
+lion = rgb toMem
   where
-    fromCore = core toCore
-    toCore = fromCore^.toMem.to bios
+    FromCore toMem _ = core $ ToCore fromBios
+    fromBios = fromCore^.toMem.to bios
     
 ----------------
 -- Top Entity --
