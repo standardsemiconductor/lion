d8512cf029d8545625cb9af5671b9a58dae4fdd0
dopamane <dwc1295@gmail.com>
2021-02-27 01:31:12 -0500

    updating readme, split formal executable from library

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# lion
-RISC-V on the VELDT
+# Where Lions Roam: RISC-V on the VELDT
+
+Lion is a formally verified 5-stage pipelined RISC-V processor core written in Haskell. Lion targets the VELDT FPGA development board.
+
+This repository contains three parts:
+  1. The Lion library: a pipelined RISC-V core.
+  2. The Lion formal verification executable: verify the core using riscv-formal.
+  3. The Lion SoC: a System-on-Chip demonstrating usage of the Lion core.
+
+## Lion library
+### Usage:
+1. Add `lion` to build depends section of Cabal file
+2. import module in source files `import Lion.Core`
+
+Building from source:
+
+## Lion formal verification
+
+## Lion SoC
+
+
---
+++ formal/Formal.hs
@@ -0,0 +1,60 @@
+module Main where
+
+import Development.Shake
+import Development.Shake.FilePath
+import Clash.Main (defaultMain)
+import System.Directory (withCurrentDirectory)
+
+formalTop :: String
+formalTop = "LionFV"
+
+buildDir :: FilePath
+buildDir = "formal/_build"
+
+buildDir' :: FilePath -> FilePath
+buildDir' = (buildDir </>)
+
+verilog :: String -> FilePath
+verilog top = "formal/_build/verilog" </> top </> top
+
+main :: IO ()
+main = shakeArgs opts $ do
+
+  phony "clean" $ do
+    putInfo "Cleaning files in _build"
+    removeFilesAfter "formal/_build" ["//*"]
+    putInfo "Cleaning riscv-formal checks"
+    removeFilesAfter "riscv-formal/cores/lion/checks" ["//*"]
+
+  phony "formal" $ do
+    putInfo "Formal Verification of Lion Core"
+    need [verilog formalTop </> formalTop <.> "v"]
+
+    -- generate checks
+    liftIO $ withCurrentDirectory "riscv-formal/cores/lion" $
+      cmd_ "python3" [".." </> ".." </> "checks" </> "genchecks.py"]
+
+    -- gather checks
+    checks <- getDirectoryFiles "riscv-formal/cores/lion/checks" ["//*.sby"]
+
+    -- verify checks
+    _ <- forP checks $ \check -> do
+      let checkNoExt = dropExtensions check
+      cmd_ "make" "-C" ["riscv-formal/cores/lion/checks"] [checkNoExt]
+      need ["riscv-formal/cores/lion/checks" </> checkNoExt </> "PASS"]
+      putInfo $ show checkNoExt ++ ": PASS"
+    putInfo "All formal checks PASS"
+
+  -- build formal 
+  verilog formalTop </> formalTop <.> "v" %> \_ ->
+    liftIO $ compile formalTop
+
+  where
+    opts = shakeOptions
+      { shakeFiles = buildDir
+      , shakeThreads = 0
+      }
+
+compile :: String -> IO ()
+compile topModule = defaultMain  ["-fclash-hdldir", buildDir, topModule, "--verilog"]
+    
