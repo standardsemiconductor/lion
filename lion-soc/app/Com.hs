3d5bcf704ed666c7742444b9b0b749f3b786c36d
dopamane <dwc1295@gmail.com>
2021-03-11 05:39:58 -0500

    user can specify port, defaults to /dev/ttyUSB0

--- lion-soc/app/Com.hs
+++ lion-soc/app/Com.hs
@@ -1,11 +1,20 @@
 
-import System.Hardware.Serialport
-import System.IO
 import Control.Concurrent ( forkIO )
 import Control.Monad      ( forever )
+import Data.Functor       ( (<&>) )
+import System.Environment ( getArgs )
+import System.Hardware.Serialport 
+import System.IO 
 
 main :: IO ()
-main = hWithSerial "/dev/ttyUSB0" serialPortSettings $ \hndl -> do
+main = com =<< portPath
+  where
+    portPath = getArgs <&> \case
+      [pathArg] -> pathArg
+      _         -> "/dev/ttyUSB0" 
+
+com :: String -> IO ()
+com portPath = hWithSerial portPath serialPortSettings $ \hndl -> do
   hSetBuffering stdin NoBuffering
   hSetBuffering stdout NoBuffering
   forkIO $ readUart hndl
