9ff3896b4a9141d5a8a5320e3484ebd03314aba7
dopamane <dwc1295@gmail.com>
2021-03-12 23:41:01 -0500

    drafting spi flash IO

--- lion-soc/src/Flash.hs
+++ lion-soc/src/Flash.hs
@@ -19,13 +19,35 @@ flash
 flash busIn = _
   where
     fromSb = sysBus toSb
-    (sbdato, sbacko, _, _, wo, woe, bo, boe, wcko, wckoe, bcsno, bcsnoe) 
+   
+    (biwo, bi) = biwoIO
+    bowi       = bowiIO boe    bo
+    wck        = wckIO  wckoe  wcko
+    cs         = csIO   bcsnoe bcsno
+
+    (sbdato, sbacko, _, _, _, _, bo, boe, wcko, wckoe, bcsno, bcsnoe) 
       = spi "0b0000"
             (getSbrwi  <$> fromSb)
             (getSbstbi <$> fromSb)
             (getSbadri <$> fromSb)
             (getSbdati <$> fromSb)
             bi
-            wi -- ??
-            wcki -- ??
-            wcsni -- ??
+            (pure 0)
+            (pure 0)
+            (pure 0)
+
+biwoIO :: HiddenClock dom => Unbundled dom (Bit, Bit)
+biwoIO = (biwo, bi)
+  where
+    (biwo, bi, _) = io PinInput
+                       PinNoOutput
+                       0 -- pullUp
+                       0 -- negTrigger
+                       SBLVCMOS
+                       0
+                       0
+                       hasClock
+                       hasClock
+                       0
+                       0
+                       0
