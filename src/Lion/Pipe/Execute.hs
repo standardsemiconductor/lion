1d06bcafcc05f70749234f2306b2ca19b86c1841
dopamane <dwc1295@gmail.com>
2021-04-03 20:39:06 -0400

    pulled haskell

--- src/Lion/Pipe/Execute.hs
+++ src/Lion/Pipe/Execute.hs
@@ -24,49 +24,152 @@ instance Ex 'Soft where
     Lui   -> MeRegWr rd imm
     Auipc -> MeRegWr rd $ pc + imm
 
+------------------
+-- Execute Jump --
+------------------
+executeJump
+  :: Jump
+  -> Unsigned 5 -- rd
+  -> BitVector 32 -- imm
+  -> BitVector 32 -- pc
+  -> BitVector 32 -- pc4
+  -> BitVector 32 -- rs1Data
+  -> RWS (ToPipe a) (FromPipe a) (Pipe a) ()
+executeJump jump rd imm pc pc4 rs1Data = case jump of
+  Jal -> do
+    npc <- meRvfi.rvfiPcWData <<~ control.exBranching <?= pc + imm
+    meRvfi.rvfiTrap ||= isMisaligned npc
+    meIR ?= MeJump rd pc4
+  Jalr -> do
+    npc <- meRvfi.rvfiPcWData <<~ control.exBranching <?= clearBit (rs1Data + imm) 0
+    meRvfi.rvfiTrap ||= isMisaligned npc
+    meIR ?= MeJump rd pc4
+
+--------------------
+-- Execute Branch --
+--------------------
+executeBranch :: RWS (ToPipe a) (FromPipe a) (Pipe a) ()
+executeBranch
+  :: Branch
+  -> BitVector 32 -- imm
+  -> BitVector 32 -- rs1Data
+  -> BitVector 32 -- rs2Data
+  -> BitVector 32 -- pc
+  -> BitVector 32 -- pc4
+  -> RWS (ToPipe a) (FromPipe a) (Pipe a) ()
+executeBranch op imm rs1Data rs2Data pc pc4 
+  | branch op rs1Data rs2Data = do
+      branchPC <- meRvfi.rvfiPcWData <<~ control.exBranching <?= pc + imm
+      meRvfi.rvfiTrap ||= isMisaligned branchPC
+      meIR ?= MeBranch
+  | otherwise = do
+      meRvfi.rvfiTrap ||= isMisaligned pc4
+      meIR ?= MeNop
+
 -------------------
--- ExecuteExJump --
+-- Execute Store --
 -------------------
-class ExecuteExJump (a :: AluConfig) where
-  executeExJump :: Jump
-                -> Unsigned 5 -- rd
-                -> BitVector 32 -- imm
-                -> BitVector 32 -- pc
-                -> BitVector 32 -- pc4
-                -> BitVector 32 -- rs1Data
-                -> RWS (ToPipe a) (FromPipe a) (Pipe a) ()
-
-class ExecuteExBranch (a :: AluConfig) where
-  executeExBranch :: RWS (ToPipe a) (FromPipe a) (Pipe a) ()
-
-class ExecuteExStore (a :: AluConfig) where
-  executeExStore 
-
-class Execute (a :: AluConfig) where
-  execute :: RWS (ToPipe a) (FromPipe a) (Pipe a) ()
-
-instance Execute (a :: AluConfig) where
-  execute = do
-    meIR .= Nothing
-    meRvfi <~ use exRvfi
-    pc  <- meRvfi.rvfiPcRData <<~ use exPC
-    pc4 <- meRvfi.rvfiPcWData <.= pc + 4
-    rs1Data <- meRvfi.rvfiRs1Data <<~ regFwd exRs1 fromRs1 (control.meRegFwd) (control.wbRegFwd)
-    rs2Data <- meRvfi.rvfiRs2Data <<~ regFwd exRs2 fromRs2 (control.meRegFwd) (control.wbRegFwd)
-    withInstr exIR $ \case
-      Ex op rd imm         -> executeEx op rd imm pc
-      ExJump jump rd imm   -> executeExJump jump rd imm pc pc4 rs1Data
-      ExBranch op imm      -> executeExBranch op imm rs1Data rs2Data pc pc4
-      ExStore op imm       -> executeExStore op imm rs1Data rs2Data
-      ExLoad op rdAddr imm -> executeExLoad op rdAddr imm rs1Data
-      ExAlu op rd          -> executeExAlu op rd rs1Data rs2Data
-      ExAluImm op rd imm   -> executeExAluImm op rd imm rs1Data
+executeStore
+  :: Store
+  -> BitVector 32 -- imm
+  -> BitVector 32 -- rs1Data
+  -> BitVector 32 -- rs2Data
+  -> RWS (ToPipe a) (FromPipe a) (Pipe a) ()
+executeStore op imm rs1Data rs2Data = case op of
+  Sb -> let wr = concatBitVector# $ replicate d4 $ slice d7 d0 rs2Data
+        in meIR ?= MeStore addr' (byteMask addr) wr
+  Sh -> do
+    meRvfi.rvfiTrap ||= isMisalignedHalf addr -- trap on half-word boundary
+    let wr = concatBitVector# $ replicate d2 $ slice d15 d0 rs2Data
+    meIR ?= MeStore addr' (halfMask addr) wr
+  Sw -> do
+    meRvfi.rvfiTrap ||= isMisaligned addr -- trap on word boundary
+    meIR ?= MeStore addr' 0xF rs2Data
   where
-    scribeAlu op in1 in2 = do
-      scribe toAluOp     $ First $ Just op
-      scribe toAluInput1 $ First $ Just in1
-      scribe toAluInput2 $ First $ Just in2
+    addr  = rs1Data + imm           -- unaligned
+    addr' = addr .&. complement 0x3 -- aligned
 
+------------------
+-- Execute Load --
+------------------
+executeLoad
+  :: Load 
+  -> Unsigned 5   -- rd
+  -> BitVector 32 -- imm
+  -> BitVector 32 -- rs1Data
+  -> RWS (ToPipe a) (FromPipe a) (Pipe a) ()
+executeLoad op rd imm rs1Data = do
+  control.exLoad .= True
+  if | op == Lb || op == Lbu -> meIR ?= MeLoad op rdAddr addr' (byteMask addr)
+     | op == Lh || op == Lhu -> do
+         meRvfi.rvfiTrap ||= isMisalignedHalf addr -- trap on half-word boundary
+         meIR ?= MeLoad op rdAddr addr' (halfMask addr)
+     | otherwise -> do -- Lw
+         meRvfi.rvfiTrap ||= isMisaligned addr -- trap on word boundary
+         meIR ?= MeLoad op rdAddr addr' 0xF
+  where
+    addr  = rs1Data + imm            -- unaligned
+    addr' = addr .&. complement 0x3 -- aligned
+
+-----------------
+-- Execute Alu --
+-----------------
+class ExecuteAlu (a :: AluConfig) where
+  executeAlu
+    :: Op
+    -> Unsigned 5   -- rd
+    -> BitVector 32 -- rs1Data
+    -> BitVector 32 -- rs2Data
+    -> RWS (ToPipe a) (FromPipe a) (Pipe a) ()
+
+instance ExecuteAlu 'Hard where  
+  executeAlu op rd rs1Data rs2Data = do
+    scribeAlu op rs1Data rs2Data
+    meIR ?= MeRegWr rd
+
+instance ExecuteAlu 'Soft where
+  executeAlu op rd rs1Data rs2Data = meIRM ?= MeRegWr rd (I.alu op rs1Data rs2Data)
+
+---------------------
+-- Execute Alu Imm --
+---------------------
+class ExecuteAluImm (a :: AluConfig) where
+  executeAluImm
+    :: Op
+    -> Unsigned 5   -- rd
+    -> BitVector 32 -- imm
+    -> BitVector 32 -- rs1Data
+    -> RWS (ToPipe a) (FromPipe a) (Pipe a) ()
+
+instance ExecuteAluImm 'Hard where
+  executeAluImm op rd imm rs1Data = do
+    scribeAlu op rs1Data imm
+    meIR ?= MeRegWr rd
+
+instance ExecuteAluImm 'Soft where
+  executeAluImm op rd imm rs1Data = meIR ?= MeRegWr rd (I.alu op rs1Data imm)
+
+execute 
+  :: ExecuteEx a 
+  => ExecuteAlu a
+  => ExecuteAluImm a
+  => RWS (ToPipe a) (FromPipe a) (Pipe a) ()
+execute = do
+  meIR .= Nothing
+  meRvfi <~ use exRvfi
+  pc  <- meRvfi.rvfiPcRData <<~ use exPC
+  pc4 <- meRvfi.rvfiPcWData <.= pc + 4
+  rs1Data <- meRvfi.rvfiRs1Data <<~ regFwd exRs1 fromRs1 (control.meRegFwd) (control.wbRegFwd)
+  rs2Data <- meRvfi.rvfiRs2Data <<~ regFwd exRs2 fromRs2 (control.meRegFwd) (control.wbRegFwd)
+  withInstr exIR $ \case
+    Ex op rd imm         -> executeEx op rd imm pc
+    ExJump jump rd imm   -> executeJump jump rd imm pc pc4 rs1Data
+    ExBranch op imm      -> executeBranch op imm rs1Data rs2Data pc pc4
+    ExStore op imm       -> executeStore op imm rs1Data rs2Data
+    ExLoad op rdAddr imm -> executeLoad op rdAddr imm rs1Data
+    ExAlu op rd          -> executeAlu op rd rs1Data rs2Data
+    ExAluImm op rd imm   -> executeAluImm op rd imm rs1Data
+  where
     guardZero  -- register x0 always has value 0.
       :: MonadState s m 
       => Lens' s (Unsigned 5) 
@@ -88,6 +191,10 @@ instance Execute (a :: AluConfig) where
     regFwd rsAddr rsData meFwd wbFwd = 
       guardZero rsAddr =<< fwd <$> use rsAddr <*> view rsData <*> use meFwd <*> use wbFwd
 
+scribeAlu op in1 in2 = do
+  scribe toAluOp     $ First $ Just op
+  scribe toAluInput1 $ First $ Just in1
+  scribe toAluInput2 $ First $ Just in2
 
 -- | Execute stage
 execute :: RWS ToPipe FromPipe (Pipe a) ()
