4e7bbd975f2d82c2a6e4ae6ad4ac4a228d6353be
dopamane <dwc1295@gmail.com>
2021-04-08 03:47:50 -0400

    working on memory stage, filling in gaps by creating PipeM monad which will contain typelevel configuration

--- src/Lion/Pipe/Memory.hs
+++ src/Lion/Pipe/Memory.hs
@@ -13,12 +13,35 @@ import Clash.Prelude
 ----------------
 -- Memory NOP --
 ----------------
+-- Pass NOP onto writeback stage.
 meNop :: PipeM (a :: AluConfig) ()
 meNop = wbIR ?= WbNop
+
+---------------------------
+-- Memory Register Write --
+---------------------------
+-- Push the ALU output and register destination address
+-- to writeback stage.
+-- Forward register with memory control.
+class MemoryRegWr (a :: AluConfig) where
+  meRegWr :: MeRegWrAlu a -> PipeM (a :: AluConfig)
+
+instance MemoryRegWr 'Hard where
+  meRegWr (MeRegWrAluHard rd) = do
+    wr <- view fromAlu
+    control.meRegFwd ?= (rd, wr)
+    wbIR ?= WbRegWr rd wr
+
+instance MemoryRegWr 'Soft where
+  meRegWr (MeRegWrAluSoft rd wr) = do
+    control.meRegFwd ?= (rd, wr)
+    wbIR ?= WbRegWr rd wr
 
 -----------------
 -- Memory Jump --
 -----------------
+-- Set memory control branching and forward register.
+-- Send register write to writeback stage.
 meJump :: Unsigned 5 -> BitVector 32 -> PipeM (a :: AluConfig) ()
 meJump rd pc4 = do
   control.meBranching .= True
@@ -28,6 +51,8 @@ meJump rd pc4 = do
 -------------------
 -- Memory Branch --
 -------------------
+-- Set memory control branching.
+-- Send NOP to writeback stage.
 meBranch :: PipeM (a :: AluConfig) ()
 meBranch = do
   control.meBranching .= True
@@ -38,65 +63,40 @@ meBranch = do
 ------------------
 meStore :: BitVector 32 -> BitVector 4 -> BitVector 32 -> PipeM (a :: AluConfig) ()
 meStore addr mask value = do
-class Memory (a :: AluConfig) where
-  memory :: RWS (ToPipe a) (FromPipe a) (Pipe a) ()
-    
-instance Memory 'Hard where
-  memory = do
-    wbIR   .= Nothing
-    wbRvfi <~ use meRvfi
-    withInstr meIR $ \case
-      MeNop -> meNop
-      MeRegWr rd -> do
-        wr <- view fromAlu
-        control.meRegFwd ?= (rd, wr)
-        wbIR ?= WbRegWr rd wr
-      MeJump rd pc4 -> meJump rd pc4
-      MeBranch -> do
-        control.meBranching .= True
-        wbIR ?= WbNop
-      MeStore addr mask value -> do
-        control.meMemory .= True
-        scribe toMem $ First $ Just $ dataMem addr mask $ Just value
-        wbRvfi.rvfiMemAddr  .= addr
-        wbRvfi.rvfiMemWMask .= mask
-        wbRvfi.rvfiMemWData .= value
-        wbIR ?= WbStore
-      MeLoad op rdAddr addr mask -> do
-        control.meMemory .= True
-        scribe toMem $ First $ Just $ dataMem addr mask Nothing
-        wbRvfi.rvfiMemAddr  .= addr
-        wbRvfi.rvfiMemRMask .= mask
-        wbIR ?= WbLoad op rdAddr mask
+  control.meMemory .= True
+  scribe toMem $ First $ Just $ dataMem addr mask $ Just value
+  wbRvfi.rvfiMemAddr  .= addr
+  wbRvfi.rvfiMemWMask .= mask
+  wbRvfi.rvfiMemWData .= value
+  wbIR ?= WbStore
 
--- | Memory stage
-memory :: RWS ToPipe FromPipe Pipe ()
+-----------------
+-- Memory Load --
+-----------------
+meLoad 
+  :: Load 
+  -> Unsigned 5 
+  -> BitVector 32 
+  -> BitVector 4 
+  -> PipeM (a :: AluConfig) ()
+meLoad op rd addr mask = do
+  control.meMemory .= True
+  scribe toMem $ First $ Just $ dataMem addr mask Nothing
+  wbRvfi.rvfiMemAddr  .= addr
+  wbRvfi.rvfiMemRMask .= mask
+  wbIR ?= WbLoad op rdAddr mask
+
+---------------------------
+-- Memory pipeline stage --
+---------------------------
+memory :: PipeM (a :: AluConfig) ()
 memory = do
   wbIR   .= Nothing
   wbRvfi <~ use meRvfi
   withInstr meIR $ \case
-    MeNop -> wbIR ?= WbNop
-    MeRegWr rd -> do
-      wr <- view fromAlu
-      control.meRegFwd ?= (rd, wr)
-      wbIR ?= WbRegWr rd wr
-    MeJump rd pc4 -> do
-      control.meBranching .= True
-      control.meRegFwd ?= (rd, pc4)
-      wbIR ?= WbRegWr rd pc4
-    MeBranch -> do
-      control.meBranching .= True
-      wbIR ?= WbNop
-    MeStore addr mask value -> do
-      control.meMemory .= True
-      scribe toMem $ First $ Just $ dataMem addr mask $ Just value
-      wbRvfi.rvfiMemAddr  .= addr
-      wbRvfi.rvfiMemWMask .= mask
-      wbRvfi.rvfiMemWData .= value
-      wbIR ?= WbStore
-    MeLoad op rdAddr addr mask -> do
-      control.meMemory .= True
-      scribe toMem $ First $ Just $ dataMem addr mask Nothing
-      wbRvfi.rvfiMemAddr  .= addr
-      wbRvfi.rvfiMemRMask .= mask
-      wbIR ?= WbLoad op rdAddr mask
+    MeNop -> meNop
+    MeRegWr regWr -> meRegWr regWr
+    MeJump rd pc4 -> meJump rd pc4
+    MeBranch -> meBranch
+    MeStore addr mask value -> meStore addr mask value
+    MeLoad op rdAddr addr mask -> meLoad op rdAddr addr mask
---
+++ src/Lion/Pipe/Monad.hs
@@ -0,0 +1,164 @@
+{-|
+Module      : Lion.Pipe.Monad
+Description : Pipeline Monad
+Copyright   : (c) David Cox, 2021
+License     : BSD-3-Clause
+Maintainer  : standardsemiconductor@gmail.com
+-}
+
+module Lion.Pipe.Monad where
+
+import Clash.Prelude
+import Control.Monad.RWS
+
+-- | Pipeline inputs
+data ToPipe = ToPipe
+  { _fromRs1 :: BitVector 32
+  , _fromRs2 :: BitVector 32
+  , _fromAlu :: BitVector 32
+  , _fromMem :: BitVector 32
+  }
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass NFDataX
+makeLenses ''ToPipe
+
+-- | Memory access - Lion has a shared instruction/memory bus
+data MemoryAccess = InstrMem -- ^ instruction access
+                  | DataMem  -- ^ data access
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass NFDataX
+
+-- | Memory bus
+data ToMem = ToMem
+  { memAccess   :: MemoryAccess         -- ^ memory access type
+  , memAddress  :: BitVector 32         -- ^ memory address
+  , memByteMask :: BitVector 4          -- ^ memory byte mask
+  , memWrite    :: Maybe (BitVector 32) -- ^ read=Nothing write=Just wr
+  }
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass NFDataX
+
+-- | Construct instruction memory access
+instrMem 
+  :: BitVector 32 -- ^ instruction address
+  -> ToMem
+instrMem addr = ToMem
+  { memAccess   = InstrMem
+  , memAddress  = addr
+  , memByteMask = 0xF
+  , memWrite    = Nothing
+  }
+
+-- | Construct data memory access
+dataMem 
+  :: BitVector 32         -- ^ memory address
+  -> BitVector 4          -- ^ byte mask
+  -> Maybe (BitVector 32) -- ^ write
+  -> ToMem
+dataMem addr mask wrM = ToMem
+  { memAccess   = DataMem
+  , memAddress  = addr
+  , memByteMask = mask
+  , memWrite    = wrM
+  }
+
+-- | Pipeline outputs
+data FromPipe = FromPipe
+  { _toMem       :: First ToMem
+  , _toRs1Addr   :: First (Unsigned 5)
+  , _toRs2Addr   :: First (Unsigned 5)
+  , _toRd        :: First (Unsigned 5, BitVector 32)
+  , _toAluOp     :: First Op
+  , _toAluInput1 :: First (BitVector 32)
+  , _toAluInput2 :: First (BitVector 32)
+  , _toRvfi      :: First Rvfi
+  }
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass NFDataX
+  deriving Semigroup via GenericSemigroup FromPipe
+  deriving Monoid via GenericMonoid FromPipe
+makeLenses ''FromPipe
+
+data Control = Control
+  { _firstCycle  :: Bool                             -- ^ First cycle True, then always False
+  , _exBranching :: Maybe (BitVector 32)             -- ^ execute stage branch/jump
+  , _meBranching :: Bool                             -- ^ memory stage branch/jump
+  , _deLoad      :: Bool                             -- ^ decode stage load
+  , _exLoad      :: Bool                             -- ^ execute stage load
+  , _meMemory    :: Bool                             -- ^ memory stage load/store
+  , _wbMemory    :: Bool                             -- ^ writeback stage load/store
+  , _meRegFwd    :: Maybe (Unsigned 5, BitVector 32) -- ^ memory stage register forwarding
+  , _wbRegFwd    :: Maybe (Unsigned 5, BitVector 32) -- ^ writeback stage register forwading
+  }
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass NFDataX
+makeLenses ''Control
+
+mkControl :: Control
+mkControl = Control 
+  { _firstCycle  = True   
+  , _exBranching = Nothing
+  , _meBranching = False
+  , _deLoad      = False
+  , _exLoad      = False
+  , _meMemory    = False
+  , _wbMemory    = False
+  , _meRegFwd    = Nothing
+  , _wbRegFwd    = Nothing
+  }
+
+data Pipe = Pipe
+  { _fetchPC :: BitVector 32
+
+  -- decode stage
+  , _dePC    :: BitVector 32
+
+  -- execute stage
+  , _exIR    :: Maybe ExInstr
+  , _exPC    :: BitVector 32
+  , _exRs1   :: Unsigned 5
+  , _exRs2   :: Unsigned 5
+  , _exRvfi  :: Rvfi
+
+  -- memory stage
+  , _meIR    :: Maybe MeInstr
+  , _meRvfi  :: Rvfi
+
+  -- writeback stage
+  , _wbIR    :: Maybe WbInstr
+  , _wbNRet  :: BitVector 64
+  , _wbRvfi  :: Rvfi
+
+  -- pipeline control
+  , _control :: Control
+  }
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass NFDataX
+makeLenses ''Pipe
+
+mkPipe :: PipeConfig -> Pipe
+mkPipe config = Pipe
+  { _fetchPC = startPC config
+
+  -- decode stage 
+  , _dePC    = 0
+  
+  -- execute stage
+  , _exIR    = Nothing
+  , _exPC    = 0
+  , _exRs1   = 0
+  , _exRs2   = 0
+  , _exRvfi  = mkRvfi
+
+  -- memory stage
+  , _meIR    = Nothing
+  , _meRvfi  = mkRvfi
+ 
+  -- writeback stage
+  , _wbIR    = Nothing
+  , _wbNRet  = 0
+  , _wbRvfi  = mkRvfi
+  
+  -- pipeline control
+  , _control = mkControl
+  }
